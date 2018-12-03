3afc6fbf7a5c29c62d6484e8b184db4232af6f00
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2018-12-03 13:33:40 -0700

    basic GET working

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,73 +4,29 @@ import MessageList from '../Message-List/Message-List'
 import Toolbar from '../Toolbar/Toolbar'
 import ComposeForm from '../Compose-Form/Compose-Form'
 
+const API = process.env.API || 'http://localhost:8082/api/messages'
+
+const getMessages = async () => {
+  const response = await fetch(`${API}`)
+  const json = await response.json()
+  return json
+}
 class App extends Component {
-constructor(){
-  super()
+constructor(props){
+  super(props)
   this.state = {
     toolBarChecked: false,
     composing: false,
-    messages: [
-  {
-    "id": 1,
-    "subject": "You can't input the protocol without calculating the mobile RSS protocol!",
-    "read": false,
-    "starred": true,
-    "labels": ["dev", "personal"]
-  },
-  {
-    "id": 2,
-    "subject": "connecting the system won't do anything, we need to input the mobile AI panel!",
-    "read": false,
-    "starred": false,
-    "selected": true,
-    "labels": []
-  },
-  {
-    "id": 3,
-    "subject": "Use the 1080p HTTP feed, then you can parse the cross-platform hard drive!",
-    "read": false,
-    "starred": true,
-    "labels": ["dev"]
-  },
-  {
-    "id": 4,
-    "subject": "We need to program the primary TCP hard drive!",
-    "read": true,
-    "starred": false,
-    "selected": true,
-    "labels": []
-  },
-  {
-    "id": 5,
-    "subject": "If we override the interface, we can get to the HTTP feed through the virtual EXE interface!",
-    "read": false,
-    "starred": false,
-    "labels": ["personal"]
-  },
-  {
-    "id": 6,
-    "subject": "We need to back up the wireless GB driver!",
-    "read": true,
-    "starred": true,
-    "labels": []
-  },
-  {
-    "id": 7,
-    "subject": "We need to index the mobile PCI bus!",
-    "read": true,
-    "starred": false,
-    "labels": ["dev", "personal"]
-  },
-  {
-    "id": 8,
-    "subject": "If we connect the sensor, we can get to the HDD port through the redundant IB firewall!",
-    "read": true,
-    "starred": true,
-    "labels": []
+    messages: []
   }
-]
-  }
+}
+
+async componentDidMount(){
+  console.log('component mounted')
+  //get data from the API
+  const response = await fetch(`${API}`)
+  const json = await getMessages()
+  this.setState({messages: json})
 }
 
 onReadClick = (e) => {
